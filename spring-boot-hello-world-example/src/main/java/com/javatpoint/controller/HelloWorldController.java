262f4b5ce0056e2daf4ba7af34cdddcdba728d20
srinivasuGit <srinivasu1243@gmail.com>
2024-06-24 17:31:34 +0530

    Updated Controller

--- spring-boot-hello-world-example/src/main/java/com/javatpoint/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -1,12 +1,18 @@
 package com.javatpoint.controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-public class HelloWorldController 
-{
-@RequestMapping("/")
-public String hello() 
-{
-return "Hello javaTpoint";
-}
+public class HelloWorldController {
+	public HelloWorldController() {
+
+	}
+
+	@GetMapping(value = "/welcome/{name}")
+	public String hello(@PathVariable("name") String name) {
+		String msg = name + " Welcome to Demo Project";
+		return msg;
+	}
 }
